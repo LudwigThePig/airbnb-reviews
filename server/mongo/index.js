47c78455eaf002c3af68799050aca1499d738949
Morgan Galvin <morgan@ludwigthepig.com>
2019-06-07 14:53:32 -0600

    update mongo index.js

--- server/mongo/index.js
+++ server/mongo/index.js
@@ -1,21 +1,27 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017/airbnb_reviews';
+const url = 'mongodb://localhost:27017/';
 
 const getReviews = (id, cb) => {
-  console.log(id);
+  const startConnection = new Date();
   MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
+    const endConnection = new Date();
+    
     if (err) {
       db.close();
       return cb(err, null);
     }
     const dbo = db.db('airbnb_reviews');
-    dbo.collection('reviews').find().skip(Number(id)).limit(10).toArray((err, documents) => {
+    const startQuery = new Date();
+    dbo.collection('reviews').find({ _id: {$gte: Number(id) } }).limit(15).toArray((err, documents) => {
       if (err){
         db.close();
         return cb(err, null);
       } 
+      const endQuery = new Date();
+      console.log('connection time ', endConnection - startConnection, 'ms');
+      console.log('query time ', endQuery - startQuery, 'ms');
+
       db.close();
-      console.log(documents)
       return cb(null, documents);
     });
   });
