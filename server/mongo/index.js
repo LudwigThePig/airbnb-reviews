f0977632e278b564be960e9a526d4d62374e8ab8
Morgan Galvin <morgan@ludwigthepig.com>
2019-06-06 19:40:10 -0600

    added skip feature to mongo

--- server/mongo/index.js
+++ server/mongo/index.js
@@ -2,14 +2,14 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017/airbnb_reviews';
 
 const getReviews = (id, cb) => {
-  console.log('you are using MongoDB');
+  console.log(id);
   MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
     if (err) {
       db.close();
       return cb(err, null);
     }
     const dbo = db.db('airbnb_reviews');
-    dbo.collection('reviews').find().limit(10).toArray((err, documents) => {
+    dbo.collection('reviews').find().skip(Number(id)).limit(10).toArray((err, documents) => {
       if (err){
         db.close();
         return cb(err, null);
